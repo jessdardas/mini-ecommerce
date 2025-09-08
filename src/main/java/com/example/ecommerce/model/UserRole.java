c43cf3b9a8e25ddc4ea3761823dc57092d36bec3
Jessica Dardas <jessicadardas@gmail.com>
2025-09-08 10:53:11 +0300

    made folders and files needed, no implemntation in them yet

--- src/main/java/com/example/ecommerce/model/UserRole.java
+++
@@ -1,7 +0,0 @@
-package com.example.ecommerce.model;
-
-// This enum defines the roles a user can have
-public enum UserRole {
-    USER,   // Regular customer
-    ADMIN   // Admin who can add products, view all orders, etc.
-}
